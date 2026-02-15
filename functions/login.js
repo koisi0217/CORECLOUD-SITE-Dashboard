338f6a8d8b26f3aa64a74e33fdaf956356bd1ebe
koisi0217 <koisi0217@gmail.com>
2026-02-15 15:12:12 +0900

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -1,12 +1,10 @@
 export async function onRequest({ env }) {
-  const params = new URLSearchParams({
-    client_id: env.DISCORD_CLIENT_ID,
-    redirect_uri: env.REDIRECT_URI,
-    response_type: "code",
-    scope: "identify guilds"
-  });
-  return Response.redirect(
-    "https://discord.com/oauth2/authorize?" + params,
-    302
-  );
+  const url =
+    "https://discord.com/api/oauth2/authorize" +
+    "?client_id=" + env.DISCORD_CLIENT_ID +
+    "&redirect_uri=https://dash.corecloud.tokyo/callback" +
+    "&response_type=code" +
+    "&scope=identify guilds";
+
+  return Response.redirect(url, 302);
 }
